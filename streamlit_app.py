8c9295ca91c596bfd96bf93aa23176e78f089304
LakshayBondwal27 <lakshaybondwal7@gmail.com>
2024-12-02 18:59:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.title('ML APP')
 st.info('This APP builds a machine learning model!')
 
 with st.expander('Data'):
-  t.write('Raw Data')
+  st.write('Raw Data')
   df = pd.read_csv('https://raw.githubusercontent.com/LakshayBondwal27/app.ML/refs/heads/master/penguins_cleaned.csv')
   df
