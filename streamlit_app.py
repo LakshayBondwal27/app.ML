51417a54d77a4258868a7bf54eabad051247697b
LakshayBondwal27 <lakshaybondwal7@gmail.com>
2024-12-03 06:44:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,34 @@ with st.expander('Data'):
   y = df.species
   y
  
+with st.expander('Data Visualization'):
+  st.scatter_chart(data = df  , x = 'bill_length_mm' , y = 'body_mass_g' , color = 'species')
+
+# Data prepraation
+
+with st.sidebar():
+  st.header('Input Features')
+  island = st.selectbox('Island' , ('Bisoe' , 'Dream' , 'Torgerson ') )
+
+  bill_length_mm = st.slider('Bill length (mm)', 32.1 , 59.6 , 43.9)
+  bill_depth_mm = st.slider('Bill length (mm)' , 13.1 , 21.5 , 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)' , 172.0 , 231.0 , 201.0)
+  body_mass_g = st.slider('Body mass (g)' , 2700.0 , 6300.0 , 4207.0)
+  gender = st.selectbox('Gender' , ('Male' , 'Female'))
+
+  #Create a Data frame
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
